a5795a7a527a0178533667fbf79894fc6951ba6b
Maressa Helena <helenamaressa@gmail.com>
2021-07-12 12:54:10 -0300

    exercicios feitos

---
+++ exercicio_2.10_1.py
@@ -0,0 +1,9 @@
+# Exercício 1) O indice de Massa Corporal (IMC) é calculado dividindo-se o peso 
+# de um individuo pelo quadrado de sua altura. Faça um programa que leia o peso e a altura
+# de um individuo e informe seu IMC
+peso = int(115)
+altura = int(2)
+IMC = (peso/(altura**2))
+print("Peso:", peso)
+print("Altura:", altura)
+print("IMC:", IMC)
